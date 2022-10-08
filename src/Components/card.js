c103f520c464c88a5d32e5c563172f2800febc86
erickmuriithi <erick.muriithi@student.moringaschool.com>
2022-10-08 15:46:28 +0300

    add file

---
+++ src/Components/card.js
@@ -0,0 +1,28 @@
+import React from 'react'
+
+const Card = ({author, description, imgUrl, name}) => {
+  return (
+    // <div>
+    //     <h1>{author}</h1>
+    //     <p>{description}</p>
+    //     <img src={imgUrl}/>
+    //     <p>{name}</p>
+    // </div>
+
+
+<div className="container-card">
+  <div className="card">
+    <div class="card__header">
+      <img src={imgUrl} alt="card__image" className="card__image" width="600" />
+    </div>
+    <div className="card__body">
+      <h4>{author}</h4>
+      <p>{description}</p>
+      <h3>{name}</h3>
+    </div>
+  </div>
+</div>
+  )
+}
+
+export default Card
