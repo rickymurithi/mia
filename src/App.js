646ec91adc149e75962a21b3b05732d1c2070ee1
erickmuriithi <erick.muriithi@student.moringaschool.com>
2022-10-08 16:51:54 +0300

    add files

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { Route, Routes } from "react-router-dom";
 
 import NavBar from './Components/NavBar';
 import Services from './Components/Services';
