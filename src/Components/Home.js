a8bcb1feffe6a5f7dd34bf98cebe54427edf8089
erickmuriithi <erick.muriithi@student.moringaschool.com>
2022-10-08 15:59:01 +0300

    add files

--- src/Components/Home.js
+++ src/Components/Home.js
@@ -1,13 +1,18 @@
 import React from 'react';
 import "./components.css";
+//import {image} from "../assets/happy"
+import happy from "../assets/happy.jpg"
 
 export default function Home() {
   return (
     <div className='home'>
       <div className='home1'>
-        <h1>MIA</h1>
+        <h1 class="font-weight-light">MIA</h1>
         <p>Your Daily guide to Wellness and Happiness.</p>
       </div>
+      <div className='image'>
+      <img src={happy} alt="happy"/>
+    </div>
     </div>
       
   )
